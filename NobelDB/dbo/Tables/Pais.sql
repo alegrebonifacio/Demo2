40f31b3a81fa4724028a00a6024e38583c3c12a0
claudio <claudio@ua.pt>
2017-10-29 16:20:59 +0000

    bd publicada e publish ok!

---
+++ NobelDB/dbo/Tables/Pais.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Pais] (
+    [PaisId]      INT           IDENTITY (1, 1) NOT NULL,
+    [Nome]        VARCHAR (100) NOT NULL,
+    [PaisAtualId] INT           NULL,
+    CONSTRAINT [PK_Pais] PRIMARY KEY CLUSTERED ([PaisId] ASC),
+    CONSTRAINT [FK_Pais_Pais] FOREIGN KEY ([PaisAtualId]) REFERENCES [dbo].[Pais] ([PaisId])
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [IX_Pais]
+    ON [dbo].[Pais]([Nome] ASC, [PaisAtualId] ASC);
+
