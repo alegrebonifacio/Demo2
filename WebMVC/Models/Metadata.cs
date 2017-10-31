0244102f860ae910e43f84018e5c80ee4170dcd9
Joaquim Sousa Pinto <jsp@ua.pt>
2017-10-31 09:09:58 +0000

    Desenvolvimentos vários ...

---
+++ WebMVC/Models/Metadata.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebMVC.Models
+{
+    public partial class PremioNobelMetadata
+    {
+        public int PremioNobelId { get; set; }
+        [Display(Name = "Year")]
+        public int Ano { get; set; }
+        [Display(Name = "Category")]
+        public int CategoriaId { get; set; }
+        [Display(Name = "Title")]
+        public string Titulo { get; set; }
+        [Display(Name = "Motivation")]
+        [DataType(DataType.MultilineText)]
+        public string Motivacao { get; set; }
+    }
+    public partial class CategoriaMetadata
+    {
+        public int CategoriaId { get; set; }
+        [Display(Name = "Category")]
+        [StringLength(100, MinimumLength = 5)]
+        [Required(ErrorMessage = "The Category must have [5-100] chars lenght")]
+        public string Nome { get; set; }
+    }
+
+}
---
+++ WebMVC/Models/PartialClasses.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebMVC.Models
+{
+
+    [MetadataType(typeof(PremioNobelMetadata))]
+    public partial class PremioNobel { }
+    [MetadataType(typeof(CategoriaMetadata))]
+    public partial class Categoria { }
+
+}
